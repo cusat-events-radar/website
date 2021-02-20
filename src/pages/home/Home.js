af75f8c54490bb8079c6d2984e54f05edabc22da
Naseem Shah <naseemshahvarikodan@gmail.com>
2021-02-20 21:00:59 +0530

    add tilt

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,32 +4,52 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import whatsAppIcon from './assets/whatsapp.svg'
 import instagramIcon from './assets/instagram.svg'
+import Tilt from 'react-parallax-tilt'
 
 export default function Home() {
   return (
     <StyledPage>
-      
-      <div className="home-header-container">
-        <img className="home-logo" src={logo} alt="CUSAT Events Radar Logo"/>
-        <div>
-          <h1 className="home-welcome">Welcome to</h1>
-          <h1 className="home-header">CUSAT</h1>
-          <h1 className="home-header">EVENTS</h1>
-          <h1 className="home-header">RADAR</h1>
-        </div>
+        <Tilt
+          className="home-parallax-tilt"
+          perspective={500}
+          glareEnable={true}
+          glareMaxOpacity={0.40}
+          scale={0.9}
+          gyroscope={true}
+          tiltAngleXInitial={0}
+          tiltAngleYInitial={0}
+          tiltMaxAngleX={8}
+          tiltMaxAngleY={8}
+          glarePosition="all"
+        >
+          <div className="home-header-container">
+            <img className="home-logo" src={logo} alt="CUSAT Events Radar Logo"/>
+            <div>
+              <h1 className="home-welcome">Welcome to</h1>
+              <h1 className="home-header">CUSAT</h1>
+              <h1 className="home-header">EVENTS</h1>
+              <h1 className="home-header">RADAR</h1>
+            </div>
 
-      </div>
-      <p className="home-sub-header">Events and opportunities in and out of CUSAT updated daily.</p>
-      <div className="home-buttons-container">
-        <button
-          onClick={()=>{window.location.href = 'https://www.instagram.com/cusat_events_radar'}}
-        ><img src={instagramIcon} alt=""/> Browse Events on Instagram</button>
-        <Link to="/code-of-conduct">
-          <button><img src={whatsAppIcon} alt=""/> Join WhatsApp Group</button>
-        </Link>
-      </div>      
+          </div>
+          <p className="home-sub-header">Events and opportunities in and out of CUSAT updated daily.</p>
+          <div className="home-buttons-container">
+            <button
+              className="home-buttons"
+              onClick={()=>{window.location.href = 'https://www.instagram.com/cusat_events_radar'}}
+            >
+              <img src={instagramIcon} alt=""/>
+              Browse Events on Instagram
+            </button>
+            <Link className="home-buttons"  to="/code-of-conduct">
+              
+                <img src={whatsAppIcon} alt=""/>
+                Join WhatsApp Group
+              
+            </Link>
+            </div>      
+        </Tilt>  
     </StyledPage>
-  
   );
 }
 
@@ -40,12 +60,13 @@ let StyledPage = styled.div`
   flex-direction: column;
   width: 100%;
   height: 100vh;
+  box-sizing: border-box;
   color: white;
-  /* padding-top: 50px; */
-  
   background: linear-gradient(218deg, #000000, #39b54a, #000000, #1d902d, #000000);
   background-size: 800% 800%;
   animation: bg-animation 20s ease-in-out infinite;
+  overflow: hidden;
+  transform-style: preserve-3d;
   @media only screen and (max-width: 650px) {
     .home-buttons-container{
       flex-direction: column;
@@ -97,18 +118,28 @@ let StyledPage = styled.div`
     50%{background-position:100% 53%}
     100%{background-position:0% 48%}
   }
+  .home-parallax-tilt{
+    padding: 25px;
+    box-sizing: border-box;
+    transform-style: preserve-3d;
+    transform: translateZ(-100px);
+  }
   .home-header-container{
     display: flex;
     align-items: center;
+    justify-content: center;
+    transform-style: preserve-3d;
   }
   .home-logo{
     width: 250px;
     box-shadow: 0 10px 100px black;
     border-radius: 50%;
     transition: all 0.5s ease-in-out;
+    transform: translateZ(50px);
     :hover{
       box-shadow: 0 0px 100px rgba(0, 255, 0, 0.535);
-      transform: scale(0.9);
+      transform: translateZ(20px);
+
     }
   }
   .home-welcome{
@@ -117,6 +148,8 @@ let StyledPage = styled.div`
     margin-left: 50px;
     font-weight: 500;
     font-size: 30;
+    transform: translateZ(50px);
+    
   }
   .home-header{
     margin: 0;
@@ -126,6 +159,7 @@ let StyledPage = styled.div`
     font-size: 60px;
     line-height: 60px;
     text-shadow: 0 5px 20px rgba(0, 0, 0, 0.446);
+    transform: translateZ(50px);
 
   }
   .home-sub-header{
@@ -134,13 +168,22 @@ let StyledPage = styled.div`
     max-width: 80%;
     text-align: center;
     margin-top: 50px;
+    margin-left: auto;
+    margin-right: auto;
+    transform: translateZ(10px);
+
+    /* white-space: nowrap;   */
   }
-  button{
+  .LinkElement{
+    transform-style: preserve-3d;
+  }
+  .home-buttons{
     display: flex;
     align-items: center;
     border: none;
     padding: 5px 20px;
     border-radius: 80px;
+    color: black;
     font-weight: 600;
     font-size: 18px;
     background-color: white;
@@ -153,6 +196,8 @@ let StyledPage = styled.div`
     border: 1.5px solid rgba(57, 181, 74, 0); 
     text-decoration: none;
     outline: none;
+    transform: translateZ(30px);
+    transform-style: preserve-3d;
     img{
       width: 20px;
       margin-right: 10px;
@@ -162,15 +207,20 @@ let StyledPage = styled.div`
       color: white;
       box-shadow: 0 0px 100px rgba(0, 255, 0, 0.535);
       border: 1.5px solid #39b54a;
+      transform: translateZ(55px);
+      transform-style: preserve-3d;
+
       img{
         filter: invert(1);
-      } 
+      }
+
 
     }
   }
   .home-buttons-container{
     display: flex;
     align-items: center;
+    transform-style: preserve-3d;
   }
 `
 
